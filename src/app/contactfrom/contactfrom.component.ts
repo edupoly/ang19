fc75435c9bdbc2b450df11871203eec3f40851ce
edupoly <info.edupoly@gmail.com>
2023-02-23 18:07:26 +0530

    formvalidations

--- src/app/contactfrom/contactfrom.component.ts
+++ src/app/contactfrom/contactfrom.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,Validators } from '@angular/forms';
 
@@ -8,16 +9,56 @@ import { FormGroup,FormControl,Validators } from '@angular/forms';
 })
 export class ContactfromComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
   contactForm:FormGroup=new FormGroup({
-    firstname:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    firstname:new FormControl('',[Validators.required,Validators.maxLength(10),this.checkFn]),
     lastname:new FormControl(),
+    username:new FormControl('',{
+      asyncValidators: [this.checkUsername.bind(this)],
+      updateOn: 'blur'
+    }),
     dob:new FormControl('',Validators.required)
   });
   ngOnInit(): void {
 
   }
+  ngDoCheck(){
+    console.log(this.contactForm)
+  }
   register(){
+    if(!this.contactForm.valid){
+      this.contactForm.markAsTouched()
+      this.contactForm.controls['firstname'].markAsTouched();
+      this.contactForm.controls['lastname'].markAsTouched();
+      this.contactForm.controls['dob'].markAsTouched();
+    }
     console.log('contactform::',this.contactForm)
+  }
+  checkFn(fn:FormControl){
+    return {} 
+  }
+  checkUsername(fn:any){
+
+    var p:any = new Promise((resolve,reject)=>{
+      this.http.get("https://fakestoreapi.com/users").subscribe((res:any)=>{
+
+        var x = res.find((u:any)=>{
+          if(u.username===fn.value){
+            return true
+          }
+          else{
+            return false;
+          }
+        })
+        if(x){
+          resolve({'usernameExist':'username already taken'})
+        }
+        else{
+          resolve(null)
+        }
+      })
+    });
+    
+    return p;
   }
 }
